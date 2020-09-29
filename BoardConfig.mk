ceb5f847c3492ae43e66833270521fed257548b1
Sonal Singh <sonal.singh.19993@gmail.com>
2020-09-29 22:32:34 +0530

    r5x: Enable real time lockscreen charging current values
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
