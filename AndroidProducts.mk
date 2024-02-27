b7fba6ceb9019635256e704de5b53fb15164d7b4
AmritoRock <amritokarmokar5i@gmail.com>
2024-02-27 12:36:53 +0600

    r5x: LineageOS Bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_r5x.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_r5x-user \
+    lineage_r5x-userdebug \
+    lineage_r5x-eng
