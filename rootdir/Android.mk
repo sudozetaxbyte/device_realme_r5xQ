kardebayan/android_device_realme_r5x

r5x: Use qcom baseband strings intead of realme baseband
Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
Co-authored-by: dev-harsh1998 <harshit@hyperteam.co.in>
Co-authored-by: Jaroslav Zviezda <acroreiser@gmail.com>
Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

rootdir/Android.mk
@@ -155,3 +155,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.oppo.reserve.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := set_baseband.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := bin/set_baseband.sh
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)

sudozetaxbyte/device_realme_r5xQ

rootdir/Android.mk
@@ -147,3 +147,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.oppo.reserve.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := set_baseband.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := etc/set_baseband.sh
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)